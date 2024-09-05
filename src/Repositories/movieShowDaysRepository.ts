77387e4e79c3495aa558ed830cc7dc9647c0f92e
arandombird671 <arandombird671@gmail.com>
2024-09-05 09:26:29 +0300

    finish movie show days repository file

--- src/Repositories/movieShowDaysRepository.ts
+++ src/Repositories/movieShowDaysRepository.ts
@@ -1,7 +1,41 @@
 import db from "../db";
 
-export async function get() {}
+type getWithFilterReqQuery = {
+  date?: string;
+  date_lt?: string;
+  date_gt?: string;
+  has_instances_with_seats_left?: string;
+  time_of_day_gt: string;
+  time_of_day_lt: string;
+};
+type DBquery = {
+  text: string;
+  values: any[];
+};
 
+export async function getById(id: string) {
+  const { rows } = await db.query(
+    "SELECT * FROM movie_show_days WHERE id = $1",
+    [id]
+  );
+  return rows[0];
+}
+
+export async function insert(movie_id: number, date: string) {
+  const results = await db.query(
+    "INSERT INTO movie_show_days (movie_id,date) VALUES($1,$2)",
+    [movie_id, date]
+  );
+  console.log(results);
+  return results.rows[0];
+}
+export async function get_movie_show_day_details(movieShowDayId: string) {
+  const { rows } = await db.query(
+    `SELECT * FROM get_movie_show_day_details($1)`,
+    [movieShowDayId]
+  );
+  return rows;
+}
 export async function get10(offset?: number) {
   const values: any[] = offset ? [offset] : [];
 
@@ -10,4 +44,93 @@ export async function get10(offset?: number) {
     values
   );
   return result.rows;
+}
+
+export async function getWithFilter(reqQuery: getWithFilterReqQuery) {
+  const query = {
+    text: "SELECT * FROM movie_show_days WHERE",
+    values: [],
+  };
+
+  helperFuncFilterByHasInstancesWithSeatsLeft(reqQuery, query);
+
+  // handle date
+  helperFuncFilterByDate(reqQuery, query);
+
+  helperFunctionFilterByTimeOfDay(reqQuery, query);
+
+  console.log(query);
+  const { rows } = await db.query(query);
+  return rows;
+}
+
+function helperFuncFilterByDate(
+  reqQuery: getWithFilterReqQuery,
+  query: DBquery
+) {
+  // search by a specific date
+  if (reqQuery.date) {
+    query.values.push(reqQuery.date);
+    query.text += ` date = ${query.values.length}`;
+  }
+  // search by a range
+  else {
+    if (reqQuery.date_gt) {
+      query.values.push(reqQuery.date_gt);
+      query.text += ` ${needsAnd(reqQuery, query) ? "AND " : ""}date > $${
+        query.values.length
+      } `;
+    }
+    if (reqQuery.date_lt) {
+      query.values.push(reqQuery.date_lt);
+      query.text += ` ${needsAnd(reqQuery, query) ? "AND " : ""}date < $${
+        query.values.length
+      } `;
+    }
+  }
+}
+
+function helperFuncFilterByHasInstancesWithSeatsLeft(
+  reqQuery: getWithFilterReqQuery,
+  query: DBquery
+) {
+  if (
+    typeof reqQuery.has_instances_with_seats_left === "undefined" ||
+    reqQuery.has_instances_with_seats_left
+  ) {
+    query.text += `${
+      query.values.length ? " AND" : ""
+    } has_instances_with_seats_left = TRUE`;
+  }
+}
+
+function helperFunctionFilterByTimeOfDay(
+  reqQuery: getWithFilterReqQuery,
+  query: DBquery
+) {
+  if (reqQuery.time_of_day_gt) {
+    query.text += `${
+      needsAnd(reqQuery, query) ? " AND" : ""
+    } filter_show_days_by_the_times_of_their_instances_gt(movie_show_days.id,'${
+      reqQuery.time_of_day_gt
+    }')`;
+  }
+  if (reqQuery.time_of_day_lt) {
+    query.text += `${
+      needsAnd(reqQuery, query) ? " AND" : ""
+    } filter_show_days_by_the_times_of_their_instances_lt(movie_show_days.id,'${
+      reqQuery.time_of_day_lt
+    }')`;
+  }
+}
+
+function needsAnd(reqQuery: getWithFilterReqQuery, query: DBquery) {
+  if (query.values.length > 0) return true;
+  if (
+    typeof reqQuery.has_instances_with_seats_left === "undefined" ||
+    reqQuery.has_instances_with_seats_left
+  )
+    return true;
+
+  return false;
 }
--- src/controller/movieShowDaysController.ts
+++ src/controller/movieShowDaysController.ts
@@ -5,19 +5,7 @@ import { validate } from "class-validator";
 import { AdminAddMovieShowDayInput } from "src/types/dto/AdminAddMovieShowDayInput";
 import { plainToInstance } from "class-transformer";
 import * as movieShowDaysRepository from "src/Repositories/movieShowDaysRepository";
-type getWithFilterReqQuery = {
-  date?: string;
-  date_lt?: string;
-  date_gt?: string;
-  has_instances_with_seats_left?: string;
-  time_of_day_gt: string;
-  time_of_day_lt: string;
-};
 
-type DBquery = {
-  text: string;
-  values: any[];
-};
 export const get: ReqHandler = async (req: { query: any }, res) => {
   const { page } = req.query;
   const offset = page ? page * 10 : 0;
@@ -28,117 +16,22 @@ export const get: ReqHandler = async (req: { query: any }, res) => {
     const showDays = await movieShowDaysRepository.get10();
     return res.json({ showDays });
   } else {
-    const showDays = await getWithFilter(req.query);
+    const showDays = await movieShowDaysRepository.getWithFilter(req.query);
     return res.json({ showDays });
   }
 };
 
 export const getById: ReqHandler = async (req, res) => {
-  const { rows } = await db.query(
-    "SELECT * FROM movie_show_days WHERE id = $1",
-    [req.params.id]
-  );
-  const showDay = rows[0];
+  const showDay = await movieShowDaysRepository.getById(req.params.id);
   return res.json({ showDay });
 };
 
-async function getWithFilter(reqQuery: getWithFilterReqQuery) {
-  const query = {
-    text: "SELECT * FROM movie_show_days WHERE",
-    values: [],
-  };
-
-  helperFuncFilterByHasInstancesWithSeatsLeft(reqQuery, query);
-
-  // handle date
-  helperFuncFilterByDate(reqQuery, query);
-
-  helperFunctionFilterByTimeOfDay(reqQuery, query);
-
-  console.log(query);
-  const { rows } = await db.query(query);
-  return rows;
-}
-
-function helperFuncFilterByDate(
-  reqQuery: getWithFilterReqQuery,
-  query: DBquery
-) {
-  // search by a specific date
-  if (reqQuery.date) {
-    query.values.push(reqQuery.date);
-    query.text += ` date = ${query.values.length}`;
-  }
-  // search by a range
-  else {
-    if (reqQuery.date_gt) {
-      query.values.push(reqQuery.date_gt);
-      query.text += ` ${needsAnd(reqQuery, query) ? "AND " : ""}date > $${
-        query.values.length
-      } `;
-    }
-    if (reqQuery.date_lt) {
-      query.values.push(reqQuery.date_lt);
-      query.text += ` ${needsAnd(reqQuery, query) ? "AND " : ""}date < $${
-        query.values.length
-      } `;
-    }
-  }
-}
-
-function helperFuncFilterByHasInstancesWithSeatsLeft(
-  reqQuery: getWithFilterReqQuery,
-  query: DBquery
-) {
-  if (
-    typeof reqQuery.has_instances_with_seats_left === "undefined" ||
-    reqQuery.has_instances_with_seats_left
-  ) {
-    query.text += `${
-      query.values.length ? " AND" : ""
-    } has_instances_with_seats_left = TRUE`;
-  }
-}
-
-function helperFunctionFilterByTimeOfDay(
-  reqQuery: getWithFilterReqQuery,
-  query: DBquery
-) {
-  if (reqQuery.time_of_day_gt) {
-    query.text += `${
-      needsAnd(reqQuery, query) ? " AND" : ""
-    } filter_show_days_by_the_times_of_their_instances_gt(movie_show_days.id,'${
-      reqQuery.time_of_day_gt
-    }')`;
-  }
-  if (reqQuery.time_of_day_lt) {
-    query.text += `${
-      needsAnd(reqQuery, query) ? " AND" : ""
-    } filter_show_days_by_the_times_of_their_instances_lt(movie_show_days.id,'${
-      reqQuery.time_of_day_lt
-    }')`;
-  }
-}
-
 // get details about a movie show day
 export const getDetails: ReqHandler = async (req, res) => {
-  const movieShowDayId = req.params.id;
-  const { rows } = await db.query(
-    `SELECT * FROM get_movie_show_day_details(${movieShowDayId})`
-  );
-  console.log(rows);
-  return res.json({ movie_show_day_details: rows });
+  const movie_show_day_details =
+    await movieShowDaysRepository.get_movie_show_day_details(req.params.id);
+  return res.json({ movie_show_day_details });
 };
-function needsAnd(reqQuery: getWithFilterReqQuery, query: DBquery) {
-  if (query.values.length > 0) return true;
-  if (
-    typeof reqQuery.has_instances_with_seats_left === "undefined" ||
-    reqQuery.has_instances_with_seats_left
-  )
-    return true;
-
-  return false;
-}
 
 //admin only
 export const add: ReqHandler = async (req, res) => {
@@ -155,14 +48,15 @@ export const add: ReqHandler = async (req, res) => {
       .json({ message: `Couldn't find a movie with id ${input.movie_id} ` });
 
   try {
-    const results = await db.query(
-      "INSERT INTO movie_show_days (movie_id,date) VALUES($1,$2)",
-      [input.movie_id, input.date]
+    const Show_Day = await movieShowDaysRepository.insert(
+      input.movie_id,
+      input.date
     );
-    console.log(results);
-    return res.status(201).json({ Show_Day: results.rows[0] });
+    return res.status(201).json({ Show_Day });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error", error });
   }
 };
+
+make other repository files
